5c4b42d0bf3f5c116d30d3546f89eb9ccb28d94e
omidhldn <omidhldn@gmail.com>
2025-03-21 18:29:01 +0330

    font-montserrat added

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function App() {
         {/* <section className='bg-(--academind-red)'>
           <h1>Get the Freedom You Deserve.</h1>  
         </section> */}
-        <section className='bg-academind-pink text-white font-display font-bold'>
+        <section className='bg-academind-pink text-white font-anton'>
           <h1>Get the Freedom You Deserve.</h1>  
         </section>  
-        <section className='bg-academind-pink text-white'>
-          <h1>Choose Your Plan</h1>  
+        <section>
+          <h1 className='text-academind-green font-anton'>Choose Your Plan</h1> 
+          <p>Make sure you get the most for your money!</p> 
         </section>  
           
       </main>  
