65a11ca98b9638db694a8884439bd347b31b59e8
omidhldn <omidhldn@gmail.com>
2025-03-21 17:07:02 +0330

    font fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ function App() {
   return (
     <>
       <main>
-        <section>
+        {/* <section className='bg-(--academind-red)'>
+          <h1>Get the Freedom You Deserve.</h1>  
+        </section> */}
+        <section className='bg-academind-pink text-white font-display font-bold'>
           <h1>Get the Freedom You Deserve.</h1>  
         </section>  
+        <section className='bg-academind-pink text-white'>
+          <h1>Choose Your Plan</h1>  
+        </section>  
+          
       </main>  
     </>
   )
