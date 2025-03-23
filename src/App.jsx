9b056f70a80cf1268dc60d3ff2d6d703350b1ffe
omidhldn <omidhldn@gmail.com>
2025-03-23 17:56:46 +0330

    hash-router considered

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
+
 import './App.css'
+import HomePage from './pages/Home';
+import About from './pages/About';
+
+const router = createHashRouter([
+  {path:'/' , element:<HomePage/>},
+  {path: '/about', element: <About/>}
+]);
 
 function App() {
 
   return (
-    <>
-      <main>
-        {/* <section className='bg-(--academind-red)'>
-          <h1>Get the Freedom You Deserve.</h1>  
-        </section> */}
-        <section className='bg-academind-pink text-white font-anton'>
-          <h1>Get the Freedom You Deserve.</h1>  
-        </section>  
-        <section>
-          <h1 className='text-academind-green'>Choose Your Plan</h1> 
-          <p>Make sure you get the most for your money!</p> 
-        </section>  
-          
-      </main>  
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+function HomePage() {
+  return (
+    <>
+      <main>
+        {/* <section className='bg-(--academind-red)'>
+          <h1>Get the Freedom You Deserve.</h1>  
+        </section> */}
+        <section className="bg-academind-pink text-white font-anton">
+          <h1>Get the Freedom You Deserve.</h1>
+        </section>
+        <section>
+          <h1 className="text-academind-green">Choose Your Plan</h1>
+          <p>Make sure you get the most for your money!</p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default HomePage;
